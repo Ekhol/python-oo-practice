bdddd83411390ce361571878b613fddb530c3a10
Nathan <frost.nathan1995@gmail.com>
2022-05-25 13:43:15 -0400

    finished assignment

--- serial.py
+++ serial.py
@@ -1,8 +1,9 @@
 """Python serial number generator."""
 
+
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -20,3 +21,15 @@ class SerialGenerator:
     100
     """
 
+    def __init__(self, start=0):
+        self.start = self.next = start
+
+    def __repr__(self):
+        return f"<SerialGenerator start={self.start} next={self.next}>"
+
+    def generate(self):
+        self.next += 1
+        return self.next - 1
+
+    def reset(self):
+        self.next = self.start
--- wordfinder.py
+++ wordfinder.py
@@ -1,5 +1,18 @@
 """Word Finder: finds random words from a dictionary."""
 
+import random
+
 
 class WordFinder:
     ...
+
+    def __init__(self, words):
+        word_file = open(words)
+        self.list = self.parse(word_file)
+        print(f"{len(self.list)} words read")
+
+    def parse(self, word_file):
+        return [word.strip() for word in word_file]
+
+    def random(self):
+        return random.choice(self.list)
